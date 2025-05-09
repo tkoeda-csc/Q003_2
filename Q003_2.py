f39a571cf7644718e19950d03b9936c1abd042e4
Tai Koeda <t.koeda@cscloud.co.jp>
2025-05-09 10:20:58 +0900

    Q003_2

---
+++ Q003_2.py
@@ -0,0 +1,18 @@
+num_candidates = int(input())
+election1 = input().split()
+election2 = input().split()
+
+same_votes = []
+
+p = 0
+
+
+for i in range(len(election1)):
+    if election1[p] == election2[p]:
+        same_votes.append(p + 1)
+    p += 1
+
+if len(same_votes) == 0:
+    print(0)
+else:
+    print(" ".join(str(v) for v in same_votes))
