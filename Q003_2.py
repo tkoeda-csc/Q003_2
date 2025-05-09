66f9afb2de4ecead18b734686dbec3af0acd8e6c
Tai Koeda <t.koeda@cscloud.co.jp>
2025-05-09 10:22:40 +0900

    edit

--- Q003_2.py
+++ Q003_2.py
@@ -9,10 +9,10 @@ p = 0
 
 for i in range(len(election1)):
     if election1[p] == election2[p]:
-        same_votes.append(p + 1)
+        same_votes.append(str(p + 1))
     p += 1
 
 if len(same_votes) == 0:
     print(0)
 else:
-    print(" ".join(str(v) for v in same_votes))
+    print(" ".join(same_votes))
